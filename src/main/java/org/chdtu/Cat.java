193b79a3f1374c21e8a699c34bbd74b534204ee4
unknown <a.v.mohylei.fitis22@chdtu.edu.ua>
2023-10-23 10:01:29 +0300

    first lab

---
+++ src/main/java/org/chdtu/Cat.java
@@ -0,0 +1,12 @@
+package org.chdtu;
+
+public class Cat implements Pet{
+
+    public Cat() {
+        System.out.println("Cat bean is created");
+    }
+    public void say() {
+        System.out.println("meow");
+    }
+
+}
